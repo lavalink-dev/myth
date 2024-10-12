9fa0abfe5d273dd5f052931e36c344ed0a131105
physic <physic@harmless.work>
2024-10-12 22:59:39 +0200

    Update vanity.py

--- cogs/vanity.py
+++ cogs/vanity.py
@@ -1,116 +1,106 @@
 import discord
-from discord.ext import commands
 import asyncpg
+
+from discord.ext import commands
+
+from tools.config import emojis, color
+from tools.context import Context
 
 class VanityRoles(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.cache = {}
 
-    @commands.group(invoke_without_command=True)
-    async def vanityroles(self, ctx):
-        await ctx.send("Available subcommands: set, string, add, remove, clear, show")
+    @commands.group(
+        description="Give out roles on a specific presence", 
+        aliases=["vanity", "vr"]
+    )
+    @commands.has_permissions(manage_channels=True)
+    async def vanityroles(self, ctx: Context):
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help(ctx.command.qualified_name)
 
-    @vanityroles.command()
-    async def set(self, ctx, status: str):
+    @vanityroles.command(
+        name="set",
+        description="Set on/off the vanityroles"
+    )
+    async def vanityroles_set(self, ctx, status: str):
         if status.lower() == "on":
             await self.client.pool.execute("INSERT INTO vanityroles (guild_id, enabled) VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET enabled = TRUE", ctx.guild.id, True)
-            await ctx.send("Vanity roles system is now **enabled**.")
+            await ctx.agree("**Enabled** the vanityroles system")
         elif status.lower() == "off":
-            await self.client.pool.execute(
-                "UPDATE vanityroles SET enabled = FALSE WHERE guild_id = $1", 
-                ctx.guild.id
-            )
-            await ctx.send("Vanity roles system is now **disabled**.")
+            await self.client.pool.execute("UPDATE vanityroles SET enabled = FALSE WHERE guild_id = $1", ctx.guild.id)
+            await ctx.agree("**Disabled** the vanityroles system")
         else:
-            await ctx.send("Please specify either `on` or `off`.")
+            await ctx.deny("**Invalid option,** use on/off")
 
     @vanityroles.command()
     async def string(self, ctx, *, vanity_string: str):
         if not vanity_string.startswith(".gg/") and not vanity_string.startswith("/"):
-            return await ctx.send("Vanity string must start with `.gg/` or `/`.")
-        await self.client.pool.execute(
-            "UPDATE vanityroles SET text = $1 WHERE guild_id = $2", 
-            vanity_string, ctx.guild.id
-        )
-        await ctx.send(f"Vanity string set to `{vanity_string}`.")
+            await ctx.send("**Vanity string** needs to start with .gg/ or /")
+            return
+            
+        await self.client.pool.execute("UPDATE vanityroles SET text = $1 WHERE guild_id = $2", vanity_string, ctx.guild.id)
+        await ctx.agree(f"**Set** the vanity string to: `{vanity_string}`")
 
     @vanityroles.command()
     async def add(self, ctx, *, role: discord.Role):
-        await self.client.pool.execute(
-            "INSERT INTO vanityroles_roles (guild_id, role_id) VALUES ($1, $2) ON CONFLICT (guild_id, role_id) DO NOTHING", 
-            ctx.guild.id, role.id
-        )
-        await ctx.send(f"Role {role.name} added for vanity role system.")
+        await self.client.pool.execute("INSERT INTO vanityroles_roles (guild_id, role_id) VALUES ($1, $2) ON CONFLICT (guild_id, role_id) DO NOTHING", ctx.guild.id, role.id)
+        await ctx.agree(f"**Added** {role.mention} to vanityroles")
 
     @vanityroles.command()
     async def remove(self, ctx, *, role: discord.Role):
-        await self.client.pool.execute(
-            "DELETE FROM vanityroles_roles WHERE guild_id = $1 AND role_id = $2", 
-            ctx.guild.id, role.id
-        )
-        await ctx.send(f"Role {role.name} removed from vanity role system.")
+        await self.client.pool.execute("DELETE FROM vanityroles_roles WHERE guild_id = $1 AND role_id = $2", ctx.guild.id, role.id)
+        await ctx.agree(f"**Added** {role.mention} from vanityroles")
 
     @vanityroles.command()
     async def clear(self, ctx):
-        await self.client.pool.execute(
-            "DELETE FROM vanityroles WHERE guild_id = $1", 
-            ctx.guild.id
-        )
-        await self.client.pool.execute(
-            "DELETE FROM vanityroles_roles WHERE guild_id = $1", 
-            ctx.guild.id
-        )
-        await ctx.send("Vanity role settings cleared for this server.")
+        await self.client.pool.execute("DELETE FROM vanityroles WHERE guild_id = $1", ctx.guild.id)
+        await self.client.pool.execute("DELETE FROM vanityroles_roles WHERE guild_id = $1", ctx.guild.id)
+        await ctx.send("**Cleared** all vanity settings")
 
     @vanityroles.command()
-    async def show(self, ctx):
-        data = await self.client.pool.fetchrow(
-            "SELECT enabled, text FROM vanityroles WHERE guild_id = $1", 
-            ctx.guild.id
-        )
-        roles = await self.client.pool.fetch(
-            "SELECT role_id FROM vanityroles_roles WHERE guild_id = $1", 
-            ctx.guild.id
-        )
+    async def settings(self, ctx):
+        data = await self.client.pool.fetchrow("SELECT enabled, text FROM vanityroles WHERE guild_id = $1", ctx.guild.id)
+        roles = await self.client.pool.fetch("SELECT role_id FROM vanityroles_roles WHERE guild_id = $1", ctx.guild.id)
 
         if data:
             status = "Enabled" if data['enabled'] else "Disabled"
             vanity_string = data['text'] or "None"
             role_names = [ctx.guild.get_role(role['role_id']).name for role in roles if ctx.guild.get_role(role['role_id'])]
-
-            await ctx.send(
-                f"**Vanity Roles Status:** {status}\n"
-                f"**Vanity String:** {vanity_string}\n"
-                f"**Assigned Roles:** {', '.join(role_names) if role_names else 'None'}"
-            )
+            
+            user_pfp = ctx.author.avatar.url if ctx.author.avatar else ctx.author.default_avatar.url
+            embed = discord.Embed(description=f"> **Vanity status:** `{status}` \n **Vanity string:** `{vanity_string}` \n **Assigned Roles:** {', '.join(role_names) if role_names else 'None'}", color=color.default)
+            embed.set_author(name=f"{ctx.author.name} | Vanityroles settings", icon_url=user_pfp)
         else:
-            await ctx.send("No vanity roles settings found for this server.")
+            await ctx.deny("**Could not** find any settings")
 
     @commands.Cog.listener()
     async def on_presence_update(self, before, after):
         if not after.guild:
             return
+        
+        guild_id = after.guild.id
 
-        data = await self.client.pool.fetchrow(
-            "SELECT * FROM vanityroles WHERE guild_id = $1 AND enabled = TRUE", 
-            after.guild.id
-        )
-
-        if not data:
+        if guild_id in self.cache:
+            cached_data = self.cache[guild_id]
+            if time.time() - cached_data['timestamp'] > 1000: 
+                cached_data = await self.update_cache(guild_id) 
+        else:
+            cached_data = await self.update_cache(guild_id)  
+        
+        if not cached_data: 
             return
 
-        vanity_string = data['text']
-        roles = await self.client.pool.fetch(
-            "SELECT role_id FROM vanityroles_roles WHERE guild_id = $1", 
-            after.guild.id
-        )
+        vanity_string = cached_data['vanity_string']
+        role_ids = cached_data['roles']
 
-        if not roles or not vanity_string:
+        if not vanity_string or not role_ids:
             return
 
         activity = after.activity.name if after.activity else ''
-        for role_data in roles:
-            role = after.guild.get_role(role_data['role_id'])
+        for role_id in role_ids:
+            role = after.guild.get_role(role_id)
             if role:
                 if vanity_string in activity:
                     if role not in after.roles:
@@ -118,6 +108,27 @@ class VanityRoles(commands.Cog):
                 else:
                     if role in after.roles:
                         await after.remove_roles(role)
+
+    async def update_cache(self, guild_id):
+        data = await self.client.pool.fetchrow(
+            "SELECT * FROM vanityroles WHERE guild_id = $1 AND enabled = TRUE", 
+            guild_id
+        )
+        if not data:
+            return None
+
+        roles = await self.client.pool.fetch(
+            "SELECT role_id FROM vanityroles_roles WHERE guild_id = $1", 
+            guild_id
+        )
+
+        self.cache[guild_id] = {
+            'vanity_string': data['text'],
+            'roles': [role['role_id'] for role in roles],
+            'timestamp': time.time() 
+        }
+
+        return self.cache[guild_id]
 
 async def setup(client):
     await client.add_cog(VanityRoles(client))
