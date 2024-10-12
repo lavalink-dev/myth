7517f1b2ff54fe6ddadc16ee0e4a6e0a1822a08b
physic <physic@harmless.work>
2024-10-12 17:07:53 +0200

    Update paginator.py

--- tools/paginator.py
+++ tools/paginator.py
@@ -1,8 +1,9 @@
+from __future__        import annotations
+
 import discord
 import uuid
 
 from discord.ext       import commands
-from __future__        import annotations
 
 from tools.config      import emoji, color
 
