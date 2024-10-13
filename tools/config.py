72a3221fcc213e9bfab770bee987b46695c6ef2d
physic <physic@harmless.work>
2024-10-13 17:52:52 +0200

    Update config.py

--- tools/config.py
+++ tools/config.py
@@ -4,12 +4,12 @@ class emoji:
     agree = "<:agree:1284945190473171056>"
     deny = "<:deny:1284945212833005568>"
     warn = "<:warn:1284945160429371544>"
-    link = "<:link:1284944950097608724>"
-    channel = "<:channel:1284955789118673078>"
-    cmd = "<:slash:1284955888070950962>"
+    link = "<:link:1295051319354331136>"
+    channel = "<:channel:1295051401012969543>"
+    cmd = "<:slash:1295051377025880124>"
 
 class color:
-    default = 0x404148
+    default = 0x483552
     warn = discord.Color.yellow()
     deny = discord.Color.red()
     agree = discord.Color.green()
