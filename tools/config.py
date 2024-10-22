6a6b0f35461481047759181492919907040cacf2
physic <totallynotphysic@gmail.com>
2024-10-22 23:21:54 +0200

    Update config.py

--- tools/config.py
+++ tools/config.py
@@ -9,7 +9,7 @@ class emoji:
     cmd = "<:slash:1295051377025880124>"
 
 class color:
-    default = 0x8AFBDD
+    default = 0x384B52
     warn = discord.Color.yellow()
     deny = discord.Color.red()
     agree = discord.Color.green()
