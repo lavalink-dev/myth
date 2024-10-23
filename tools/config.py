5a5c7e33ade55c18135555de7848840c79ae406b
physic <totallynotphysic@gmail.com>
2024-10-23 22:06:36 +0200

    Update config.py

--- tools/config.py
+++ tools/config.py
@@ -4,9 +4,9 @@ class emoji:
     agree = "<:agree:1284945190473171056>"
     deny = "<:deny:1284945212833005568>"
     warn = "<:warn:1284945160429371544>"
-    link = "<:link:1295051319354331136>"
-    channel = "<:channel:1295051401012969543>"
-    cmd = "<:slash:1295051377025880124>"
+    link = "🔗"
+    channel = "<:channel:1298737537682247763>"
+    cmd = "<:slash:1298737728850100339>"
 
 class color:
     default = 0xE8E4FD
