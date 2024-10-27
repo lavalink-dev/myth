b68a89665cfab6619850860e3d00363cd345ae94
physic <totallynotphysic@gmail.com>
2024-10-27 10:14:59 +0100

    Update net.py

--- cogs/net.py
+++ cogs/net.py
@@ -1,6 +1,10 @@
 import discord
-from discord.ext import commands
-from fulcrum_api import FulcrumAPI
+
+from discord.ext       import commands
+from fulcrum_api       import FulcrumAPI
+
+from tools.context     import Context
+from tools.config      import emoji, color
 
 class Network(commands.Cog):
     def __init__(self, client):
