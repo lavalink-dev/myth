2652c3b15f5a6c62490551ca980b26bb92ef420f
physic <totallynotphysic@gmail.com>
2024-10-27 10:18:44 +0100

    Update net.py

--- cogs/net.py
+++ cogs/net.py
@@ -15,10 +15,16 @@ class Network(commands.Cog):
     async def tiktok(self, ctx, username: str):
         data = await self.fulcrumapi.tiktok_user(username)
         embed = discord.Embed(color=color.default, title=f"{data['nickname']} {data['username']}")
-        embed.set_thumbnail(url=data["profile_picture"])
-        embed.add_field(name="followers", value=data["followers"])
-        embed.add_field(name="following", value=data["following"])
-        embed.add_field(name="videos", value=data["videos"])
+        embed.set_thumbnail(url=data["avatar"])
+        embed.add_field(name="ID", value=data["id"])
+        embed.add_field(name="Bio", value=data["bio"], inline=False)
+        embed.add_field(name="Verified", value="Yes" if data["verified"] else "No")
+        embed.add_field(name="Private", value="Yes" if data["private"] else "No")
+        embed.add_field(name="Following", value=data["following"])
+        embed.add_field(name="Followers", value=data["followers"])
+        embed.add_field(name="Hearts", value=data["hearts"])
+        embed.add_field(name="Videos", value=data["videos"])
+        embed.add_field(name="Profile URL", value=data["url"], inline=False)
         await ctx.send(embed=embed)
 
 async def setup(client):
