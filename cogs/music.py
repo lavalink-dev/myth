c895127da368b87edf7639abf98f612272447fb6
physic <totallynotphysic@gmail.com>
2024-10-27 18:35:16 +0100

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -1,37 +1,30 @@
 import discord
 from discord.ext import commands
 import wavelink
-from typing import Optional
 
 class Music(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.bot.loop.create_task(self.setup_hook())
+    def __init__(self, client):
+        self.client = client
+        self.client.loop.create_task(self.setup_hook())
 
     async def setup_hook(self):
-        """Connect to our Lavalink nodes."""
-        await self.bot.wait_until_ready()
-        
-        node: wavelink.Node = wavelink.Node(
-            uri='http://localhost:2333', # Your lavalink server
-            password='securepassfr'   # From your application.yml
+        node = wavelink.Node(
+            uri='http://localhost:2333',
+            password='securepassfr'
         )
-        await wavelink.NodePool.connect(client=self.bot, nodes=[node])
+        await wavelink.Pool.connect(nodes=[node], client=self.client)
 
     @commands.Cog.listener()
     async def on_wavelink_node_ready(self, node: wavelink.Node):
-        """Event fired when a node has finished connecting."""
         print(f'Node {node.identifier} is ready!')
 
     @commands.command()
     async def play(self, ctx: commands.Context, *, search: str):
-        """Play a song with the given search query."""
         if not ctx.voice_client:
             vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
         else:
             vc: wavelink.Player = ctx.voice_client
 
-        # Search for the track
         track = await wavelink.YouTubeTrack.search(search, return_first=True)
         
         if not track:
@@ -42,7 +35,6 @@ class Music(commands.Cog):
 
     @commands.command()
     async def stop(self, ctx: commands.Context):
-        """Stop the currently playing song."""
         if not ctx.voice_client:
             return await ctx.send("Not connected to a voice channel.")
             
@@ -52,7 +44,6 @@ class Music(commands.Cog):
 
     @commands.command()
     async def pause(self, ctx: commands.Context):
-        """Pause the currently playing song."""
         if not ctx.voice_client:
             return await ctx.send("Not connected to a voice channel.")
             
@@ -66,7 +57,6 @@ class Music(commands.Cog):
 
     @commands.command()
     async def resume(self, ctx: commands.Context):
-        """Resume the currently paused song."""
         if not ctx.voice_client:
             return await ctx.send("Not connected to a voice channel.")
             
@@ -80,7 +70,6 @@ class Music(commands.Cog):
 
     @commands.command()
     async def volume(self, ctx: commands.Context, vol: int):
-        """Change the player's volume."""
         if not ctx.voice_client:
             return await ctx.send("Not connected to a voice channel.")
             
@@ -93,7 +82,6 @@ class Music(commands.Cog):
 
     @commands.command()
     async def skip(self, ctx: commands.Context):
-        """Skip the currently playing song."""
         if not ctx.voice_client:
             return await ctx.send("Not connected to a voice channel.")
             
@@ -107,7 +95,6 @@ class Music(commands.Cog):
 
     @commands.command()
     async def disconnect(self, ctx: commands.Context):
-        """Disconnect the bot from the voice channel."""
         if not ctx.voice_client:
             return await ctx.send("Not connected to a voice channel.")
             
@@ -116,12 +103,11 @@ class Music(commands.Cog):
 
     @play.before_invoke
     async def ensure_voice(self, ctx: commands.Context):
-        """Ensure the bot and user are in a voice channel."""
         if not ctx.author.voice:
             raise commands.CommandError("You are not connected to a voice channel.")
         
         if ctx.voice_client and ctx.voice_client.channel != ctx.author.voice.channel:
             raise commands.CommandError("Bot is already in a voice channel.")
 
-async def setup(bot):
-    await bot.add_cog(Music(bot))
+async def setup(client):
+    await client.add_cog(Music(client))
