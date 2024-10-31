db71c101d29d2e7f2d2a3e33119d505c1aac2671
physic <totallynotphysic@gmail.com>
2024-10-31 17:59:55 +0100

    Update information.py

--- cogs/information/information.py
+++ cogs/information/information.py
@@ -39,51 +39,69 @@ class Information(CogMeta):
                 Button(
                     style=ButtonStyle.link,
                     label="support",
-                    url="https://discord.gg/uid"
-                )
+                    url="https://discord.gg/uid",
+                    emoji=emoji.link
+            )
             ).add_item(
                 Button(
                     style=ButtonStyle.link,
                     label="invite me",
-                    url=f"https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot+applications.commands&permissions=8" # type: ignore
+                    url=f"https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot+applications.commands&permissions=8", # type: ignore
+                    emoji=emoji.link
                 )
             )
         )
     
     @command(aliases=("bi", "bot"))
-    async def botinfo(self, ctx: Context) -> Message:
-        """ Display information about the bot """
+    async def botinfo(self, ctx: Context):
+        """Display information about the bot"""
 
         guild_count = len(self.bot.guilds)
         latency = round(self.bot.latency * 1000)
 
-        return await ctx.send(
-            embed=Embed(
-                title="Information",
-                description=f"> Developed by [lavalink](https://github.com/lavalink-dev) & [misimpression](https://github.com/misimpression)\n> Python version: {python_version()}",
-                color=Color.default
-            ).add_field(
-                name="Statistics",
-                value=(
-                    f"> **Latency:** `{latency}ms`\n"
-                    f"> **Commands:** `{len(self.bot.public_commands)}`\n" # type: ignore
-                    f"> **Guilds:** `{guild_count}`\n"
-                    f"> **Users:** `{len(self.bot.members):,}`"
-                )
-            ).add_field(
-                name="Other Information",
-                value=(
-                    f"> **GPU Usage:** `{cpu_percent()}%`\n"
-                    f"> **CPU Usage:** `{virtual_memory().percent}%`\n"
-                    f"> Support: [click here](https://discord.gg/uid)\n"
-                    f"> **Discord.py:** {__version__}"
-                )
-            ).set_footer(
-                text="Myth v1.2"
-            ).set_thumbnail(
-                url=self.bot.user.display_avatar.url # type: ignore
-            ).set_author(
-                name=ctx.author.name,
-                icon_url=ctx.author.display_avatar.url # type: ignore
+        view = View(timeout=None).add_item(
+            Button(
+                style=ButtonStyle.link,
+                label="support",
+                url="https://discord.gg/uid",
+                emoji=emoji.link
+            )
+        ).add_item(
+            Button(
+                style=ButtonStyle.link,
+                label="invite me",
+                url=f"https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&scope=bot+applications.commands&permissions=8",  # type: ignore
+                emoji=emoji.link
             )
         )
+
+        embed = Embed(
+            title="Information",
+            description=f"> Developed by [lavalink](https://github.com/lavalink-dev) & [misimpression](https://github.com/misimpression)",
+            color=Color.default()
+        ).add_field(
+            name="Statistics",
+            value=(
+                f"> **Latency:** `{latency}ms`\n"
+                f"> **Commands:** `{len(self.bot.public_commands)}`\n"  # type: ignore
+                f"> **Guilds:** `{guild_count}`\n"
+                f"> **Users:** `{len(self.bot.members):,}`"
+            )
+        ).add_field(
+            name="Other Information",
+            value=(
+                f"> **GPU Usage:** `{cpu_percent()}%`\n"
+                f"> **CPU Usage:** `{virtual_memory().percent}%`\n"
+                f"> **Python**: {python_version()}\n"
+                f"> **Discord.py:** {__version__}"
+            )
+        ).set_footer(
+            text="Myth v1.2"
+        ).set_thumbnail(
+            url=self.bot.user.display_avatar.url  # type: ignore
+        ).set_author(
+            name=ctx.author.name,
+            icon_url=ctx.author.display_avatar.url  # type: ignore
+        )
+
+        await ctx.send(embed=embed, view=view)
