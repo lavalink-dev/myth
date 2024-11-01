70ce24c6f52b1f11abf17be9fb6bac55fb87a6a4
physic <totallynotphysic@gmail.com>
2024-11-01 21:46:57 +0100

    Recoloured the bot

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class emoji:
     deny = "<:deny:1284945212833005568>"
     warn = "<:warn:1284945160429371544>"
     link = "🔗"
-    channel = "<:channel:1298737537682247763>"
-    cmd = "<:slash:1298737728850100339>"
+    channel = "<:channel:1302010800218837062>"
+    cmd = "<:slash:1302010819659174039>"
 
 class color:
     default = 0xE8E4FD
@@ -16,10 +16,10 @@ class color:
 
 class Emojis:
     class Paginator:
-        left = '<:left:1294716353952874547>'
-        right = '<:right:1294716290199720037>'
-        navigate = '<:paginate:1294716310701215784>'
-        cancel = '<:exit:1294716331538645044>'
+        left = "<:left:1294716353952874547>"
+        right = "<:right:1294716290199720037>"
+        navigate = "<:paginate:1294716310701215784>"
+        cancel = "<:exit:1294716331538645044>"
 
 class Color:
-    default = 0xE8E4FD
+    default = 0xA8A8A8
