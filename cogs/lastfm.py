4bd9af6260151bf26a90d832a09cdeccc880d0e1
physic <totallynotphysic@gmail.com>
2024-11-02 14:37:07 +0100

    Update lastfm.py

--- cogs/lastfm.py
+++ cogs/lastfm.py
@@ -19,26 +19,32 @@ class LastFm(commands.Cog):
                 track_name = now_playing["name"]
                 album = now_playing.get("album", {}).get("#text", "Unknown Album")
                 url = now_playing.get("url", "No URL")
-                return f"**{artist} - {track_name}**\nAlbum: {album}\n[Link to track]({url})"
-            return "No currently playing track found."
+                return artist, track_name, album, url
+            return None, None, None, None
 
     async def fetch_top_week(self, username):
         url = f"http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user={username}&api_key={self.api_key}&format=json&period=7day&limit=10"
         async with self.session.get(url) as response:
             data = await response.json()
             top_tracks = data["toptracks"]["track"]
-            if top_tracks:
-                result = []
-                for track in top_tracks:
-                    artist = track["artist"]["name"]
-                    track_name = track["name"]
-                    play_count = track["playcount"]
-                    result.append(f"**{artist} - {track_name}** ({play_count} plays)")
-                return "\n".join(result)
-            return "No top tracks found for the past week."
+            return [
+                {
+                    "artist": track["artist"]["name"],
+                    "track_name": track["name"],
+                    "play_count": track["playcount"]
+                }
+                for track in top_tracks
+            ] if top_tracks else None
+
+    async def fetch_artist_playcount(self, username, artist_name):
+        url = f"http://ws.audioscrobbler.com/2.0/?method=user.getartisttracks&user={username}&artist={artist_name}&api_key={self.api_key}&format=json"
+        async with self.session.get(url) as response:
+            data = await response.json()
+            artist_tracks = data["artisttracks"]["track"]
+            return sum(int(track["playcount"]) for track in artist_tracks) if artist_tracks else 0
 
     @commands.group(
-        description="Filter out bad words", 
+        description="Last.fm integration", 
         aliases=["lf", "fm"]
     )
     async def lastfm(self, ctx: commands.Context):
@@ -47,7 +53,7 @@ class LastFm(commands.Cog):
 
     @lastfm.command(
         name="nowplaying",
-        description="Check what's playing currently",
+        description="Check what's currently playing",
         aliases=["np"]
     )
     async def lastfm_nowplaying(self, ctx):
@@ -55,8 +61,18 @@ class LastFm(commands.Cog):
         if not username:
             await ctx.send("You haven't set a Last.fm username. Use `!lastfm set <username>` to set one.")
         else:
-            np = await self.fetch_now_playing(username)
-            await ctx.send(f"Now playing:\n{np}")
+            artist, track_name, album, url = await self.fetch_now_playing(username)
+            if artist:
+                embed = discord.Embed(
+                    title="Now Playing",
+                    description=f"[{artist} - {track_name}]({url})",
+                    color=discord.Color.blue()
+                )
+                embed.add_field(name="Album", value=album, inline=False)
+                embed.set_footer(text=f"Requested by {ctx.author.display_name}")
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send("No currently playing track found.")
 
     @lastfm.command(
         name="topweek",
@@ -68,8 +84,40 @@ class LastFm(commands.Cog):
         if not username:
             await ctx.send("You haven't set a Last.fm username. Use `!lastfm set <username>` to set one.")
         else:
-            top_week = await self.fetch_top_week(username)
-            await ctx.send(f"Top tracks this week:\n{top_week}")
+            top_tracks = await self.fetch_top_week(username)
+            if top_tracks:
+                embed = discord.Embed(
+                    title="Top Tracks This Week",
+                    color=discord.Color.green()
+                )
+                for track in top_tracks:
+                    embed.add_field(
+                        name=f"{track['artist']} - {track['track_name']}",
+                        value=f"{track['play_count']} plays",
+                        inline=False
+                    )
+                embed.set_footer(text=f"Requested by {ctx.author.display_name}")
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send("No top tracks found for the past week.")
+
+    @lastfm.command(
+        name="artist",
+        description="Show your play count for a specific artist",
+    )
+    async def lastfm_artist(self, ctx, *, artist_name: str):
+        username = await self.client.pool.fetchval("SELECT username FROM lastfm WHERE user_id = $1", ctx.author.id)
+        if not username:
+            await ctx.send("You haven't set a Last.fm username. Use `!lastfm set <username>` to set one.")
+        else:
+            play_count = await self.fetch_artist_playcount(username, artist_name)
+            embed = discord.Embed(
+                title=f"{artist_name} Play Count",
+                description=f"You have listened to **{artist_name}** {play_count} times.",
+                color=discord.Color.purple()
+            )
+            embed.set_footer(text=f"Requested by {ctx.author.display_name}")
+            await ctx.send(embed=embed)
 
     @lastfm.command(
         name="set",
