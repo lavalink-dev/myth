8a7ad2e81301f60cb2be94265f2f0e33d8992732
physic <totallynotphysic@gmail.com>
2024-11-02 14:20:02 +0100

    Update lastfm.py

--- cogs/lastfm.py
+++ cogs/lastfm.py
@@ -1,29 +1,11 @@
 import discord
-from discord.ext import commands, tasks
-import asyncpg
+from discord.ext import commands
 import aiohttp
 
 class LastFm(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, client):
+        self.client = client
         self.api_key = "4d2d1157ada4114d6cacd34fc0e58532"
-        self.session = aiohttp.ClientSession()
-
-    async def get_lastfm_username(self, user_id):
-        query = "SELECT lastfm_username FROM users WHERE user_id = $1"
-        return await self.bot.db.fetchval(query, user_id)
-
-    async def set_lastfm_username(self, user_id, username):
-        query = """
-        INSERT INTO users (user_id, lastfm_username)
-        VALUES ($1, $2)
-        ON CONFLICT (user_id) DO UPDATE SET lastfm_username = $2
-        """
-        await self.bot.db.execute(query, user_id, username)
-
-    async def remove_lastfm_username(self, user_id):
-        query = "UPDATE users SET lastfm_username = NULL WHERE user_id = $1"
-        await self.bot.db.execute(query, user_id)
 
     async def fetch_now_playing(self, username):
         url = f"http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={username}&api_key={self.api_key}&format=json&limit=1"
@@ -37,24 +19,35 @@ class LastFm(commands.Cog):
                 return f"{artist} - {track_name}"
             return "No currently playing track found."
 
-    @commands.command()
+    @commands.group(invoke_without_command=True)
+    async def lastfm(self, ctx):
+        await ctx.send("Available subcommands: `nowplaying`, `set <username>`, `remove`.")
+
+    @lastfm.command()
     async def nowplaying(self, ctx):
-        username = await self.get_lastfm_username(ctx.author.id)
+        username = await self.client.pool.fetchval("SELECT username FROM lastfm WHERE user_id = $1", ctx.author.id)
         if not username:
             await ctx.send("You haven't set a Last.fm username. Use `!lastfm set <username>` to set one.")
         else:
             now_playing = await self.fetch_now_playing(username)
             await ctx.send(f"Now playing: {now_playing}")
 
-    @commands.command()
-    async def lfset(self, ctx, username: str):
-        await self.set_lastfm_username(ctx.author.id, username)
+    @lastfm.command()
+    async def set(self, ctx, username: str):
+        await self.client.pool.execute(
+            """
+            INSERT INTO lastfm (user_id, username)
+            VALUES ($1, $2)
+            ON CONFLICT (user_id) DO UPDATE SET username = $2
+            """,
+            ctx.author.id, username
+        )
         await ctx.send(f"Your Last.fm username has been set to {username}.")
 
-    @commands.command()
-    async def lfremove(self, ctx):
-        await self.remove_lastfm_username(ctx.author.id)
+    @lastfm.command()
+    async def remove(self, ctx):
+        await self.client.pool.execute("UPDATE lastfm SET username = NULL WHERE user_id = $1", ctx.author.id)
         await ctx.send("Your Last.fm username has been removed.")
 
-async def setup(bot):
-    await bot.add_cog(LastFm(bot))
+async def setup(client):
+    await client.add_cog(LastFm(client))
